4e28cd4fb95a50c2851a69924ec0bf35b9b09ef0
avaarm <avaarm95@gmail.com>
2022-08-30 12:40:59 -0700

    general edits/ change of profile photo

--- src/components/AboutContainer/index.js
+++ src/components/AboutContainer/index.js
@@ -12,11 +12,11 @@ function AboutContainer() {
                     <div id="about-me"><img src={profile} width="100" height="100" alt="profile picture"></img>
                      <Header title="About Me" />
                         <p className="uk-text-justify about-me">
-                        Full-stack web developer with a background in immunology and preclinical research. 
+                        Hi, I'm Dev Bun, a full-stack developer with a background in immunology research. 
                         I began my journey into programming in order to create access to tech for young women,
                         who do not have the opportunity or tools to learn programming or computer science. 
                         I now hope to work in the tech industry and along with working on new, exciting technology, gain knowledge and experience, 
-                        so I can better inform young Armenian women who are also interested in technology
+                        so I can better inform other women who are also interested in technology
                         </p>
                     </div>
                 </div>
